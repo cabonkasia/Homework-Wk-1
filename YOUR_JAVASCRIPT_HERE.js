263799b00c25ae33629fb3c43f865f3774287d31
cabonkasia <cabonkc@gmail.com>
2018-10-20 17:04:05 +0200

    added displayStats function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -47,3 +47,33 @@ document.getElementById('bag').addEventListener('click', function(heroLikeObj) {
     equipWeapon(hero);   
 })
 
+//---BONUS---// 
+//---displayStats function---//
+
+    function displayStats(heroLikeObj) {
+        let name = heroLikeObj.name;
+        let health = heroLikeObj.health;
+        let weaponType = heroLikeObj.weapon.type;
+        let weaponDamage = heroLikeObj.weapon.damage;
+        let displayStatsMessage = `Hero\'s name: ${name}, hero\'s health: ${health}, hero\'s weapon: ${weaponType}, hero\'s weapon damage: ${weaponDamage}`
+
+        document.getElementById('displayStats').innerHTML = displayStatsMessage;
+   } 
+
+ 
+
+ //---change name function---//
+
+ function changeName (heroLikeObj) {
+        let input = document.querySelector('input');
+        console.log('changeName function works')
+        document.getElementById('submitBtn').addEventListener('click', function() {
+            heroLikeObj.name = input;
+        });
+        
+ };
+
+ changeName(hero);
+
+ //---the end---//
+ displayStats(hero);
